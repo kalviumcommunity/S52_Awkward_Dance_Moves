8f3f6175345098a84ffe201f5f422efc58013d3e
Mugilan Sakthivel <mugilansakthivel@Mugilans-MacBook-Pro.local>
2024-02-04 22:51:10 +0530

    there is 2 folder created which called .env for holding the uri of my mongooes and db.js for connecting with my data base and i accessed these thing in the server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
+const {ConnecttoDB,disconnecttoDB, isConnected}= require('./db')
 
 app.get('/ping' , (req, res) => {
     res.json({message:"Pong"})
 })
 
+app.get('/', (req , res) => {
+    if(isConnected){
+        res.json({connection_state: "connected"})
+    }else{
+        res.json({connection_state: "disconnected"})
+    }
+})
 
 app.listen(3000, () => {
+    ConnecttoDB()
     console.log("this server is running in 3000") 
 })
